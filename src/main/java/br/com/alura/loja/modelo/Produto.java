acd3740098967112fb2d246519cc0413583eea36
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-03-13 16:52:39 -0300

    Mapeamento de entidades com herança JPA

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -8,6 +8,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -16,6 +18,8 @@ import javax.persistence.Table;
 @Table(name = "produtos")
 @NamedQuery(name = "Produto.buscarPorNomeDaCategoria", 
 query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome")
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // Cria novos atributos na mesma entidade em que a subclasses estão herdando.
+@Inheritance(strategy = InheritanceType.JOINED) // Cria novas tabela que estão herdando da entidade pai.
 public class Produto {
 
 	@Id
